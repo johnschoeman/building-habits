1d5983fe33817a0fdc576bc5762096e2bec33fcb
Devin Jameson <devinjameson@me.com>
2020-10-04 15:36:49 -0400

    Add undo complete habit and make visual updates
    - Add undo functionality that removes the last habit entry
    - Swap thumbs up icon for pressable undo icon
    - Swap "Save" text on edit screen for save icon
    - Remove some unused imports
    - Make other minor design tweaks

--- src/Icons.elm
+++ src/Icons.elm
@@ -13,20 +13,22 @@ check color size =
         ]
 
 
-edit : String -> Int -> Html msg
-edit color size =
+undo : String -> Int -> Html msg
+undo color size =
     svgFeatherIcon color
         size
-        [ Svg.path [ d "M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" ] []
-        , Svg.path [ d "M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" ] []
+        [ Svg.polyline [ points "1 4 1 10 7 10" ] []
+        , Svg.path [ d "M3.51 15a9 9 0 1 0 2.13-9.36L1 10" ] []
         ]
 
 
-thumbsUp : String -> Int -> Html msg
-thumbsUp color size =
+save : String -> Int -> Html msg
+save color size =
     svgFeatherIcon color
         size
-        [ Svg.path [ d "M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3" ] []
+        [ Svg.path [ d "M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z" ] []
+        , Svg.polyline [ points "17 21 17 13 7 13 7 21" ] []
+        , Svg.polyline [ points "7 3 7 8 15 8" ] []
         ]
 
 
